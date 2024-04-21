d5c1efbc711cd8cc1d656a28395d3bbc30636100
tnangpal <tanyanangpal@gmail.com>
2024-04-21 13:21:05 -0500

    sudoku image added

--- src/SudokuGame.java
+++ src/SudokuGame.java
@@ -2,12 +2,14 @@ import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.GraphicsText;
 import edu.macalester.graphics.Image;
 
+import java.awt.Color;
+
 public class SudokuGame {
     private static final int CANVAS_WIDTH = 600;
     private static final int CANVAS_HEIGHT = 800;
     private static int lives = 3;
     private CanvasWindow canvas;  
-    private Image board = new Image(100, 300,"[CHANGE TO IMAGE FILE NAME]"); // FIX ME
+    private Image board = new Image(100, 300,"Sudoku.png"); // FIX ME
 
     public SudokuGame(){
         canvas = new CanvasWindow("Sudoku!", CANVAS_WIDTH,CANVAS_HEIGHT);
